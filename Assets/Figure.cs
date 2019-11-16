61004d677f1abfbf96bc05699dc2a1584fcdb4c1
Alex Melyon <alexmelyon@gmail.com>
2019-11-17 04:12:07 +0500

    init

---
+++ Assets/Figure.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Figure : MonoBehaviour
+{
+
+    /// <summary>
+    /// Sent when an incoming collider makes contact with this object's
+    /// collider (2D physics only).
+    /// </summary>
+    /// <param name="other">The Collision2D data associated with this collision.</param>
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("COLLISION");
+        Rigidbody2D rigid = GetComponent<Rigidbody2D>();
+        rigid.constraints = RigidbodyConstraints2D.FreezeAll;
+
+        transform.position = new Vector3(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y), transform.position.z);
+        
+    }
+}
---
+++ Assets/Sling.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sling : MonoBehaviour
+{
+    public GameObject origin;
+    public GameObject arrow;
+    public float force = 10;
+    public GameObject figure;
+    public GameObject[] figures;
+    
+
+
+    void Start()
+    {
+        if(figure == null) {
+            CreateFigure();
+        }
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButton(0)) {
+            handleClick(Input.mousePosition);
+        }
+        if(Input.GetMouseButtonUp(0)) {
+            handleRelease(Input.mousePosition);
+        }
+        foreach(Touch t in Input.touches) {
+            if(t.phase == TouchPhase.Began || t.phase == TouchPhase.Moved) {
+                handleClick(t.position);
+            } else if(t.phase == TouchPhase.Ended) {
+                handleRelease(t.position);
+            }
+        }
+    }
+    void handleClick(Vector3 pos) {
+        Vector3 dir = getDir(pos);
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        arrow.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    Vector3 getDir(Vector3 pos) {
+        Vector3 originPos = origin.transform.position;
+        Vector3 world = Camera.main.ScreenToWorldPoint(pos);
+        Vector3 dir = world - originPos;
+        dir = -dir * 2;
+        return dir;
+    }
+
+    void handleRelease(Vector3 pos) {
+        Rigidbody2D rigid = figure.GetComponent<Rigidbody2D>();
+        rigid.constraints = RigidbodyConstraints2D.FreezeRotation;
+        
+        Vector3 dir = getDir(pos);
+        rigid.AddForce(dir * force, ForceMode2D.Impulse);
+        
+        CreateFigure();
+    }
+
+    void CreateFigure() {
+        StartCoroutine(InstantiateCoroutine());
+    }
+
+    IEnumerator InstantiateCoroutine() {
+        yield return new WaitForSeconds(0.5F);
+        figure = Instantiate(figures[Random.Range(0, 5)], origin.transform.position, Quaternion.identity);
+        figure.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
+        yield break;
+    }
+}
